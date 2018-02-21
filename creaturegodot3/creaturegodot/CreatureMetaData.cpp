f91de618a4b7781e63441985313000d79f90fd9f
kestrelm <creature@kestrelmoon.com>
2018-02-20 21:20:33 -0800

    Starting Morph Targets Framework

--- creaturegodot3/creaturegodot/CreatureMetaData.cpp
+++ creaturegodot3/creaturegodot/CreatureMetaData.cpp
@@ -1,10 +1,13 @@
 #include "CreatureMetaData.h"
 #include "gason.h"
 #include "MeshBone.h"
+#include "CreatureModule.h"
 #include <memory>
 #include <algorithm>
 #include <iostream>
+#include <cstring>
 
+// Helper functions
 
 class JsonDataHelper
 {
@@ -14,6 +17,59 @@ public:
     JsonAllocator allocator;
 };
 
+namespace Base64Lib {
+	static const std::string base64_chars =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		"abcdefghijklmnopqrstuvwxyz"
+		"0123456789+/";
+
+	static inline bool is_base64(uint8_t c) {
+		return (isalnum(c) || (c == '+') || (c == '/'));
+	}
+
+	std::vector<uint8_t> base64_decode(const std::string& encoded_string) {
+		int in_len = static_cast<int>(encoded_string.size());
+		int i = 0;
+		int j = 0;
+		int in_ = 0;
+		uint8_t char_array_4[4], char_array_3[3];
+		std::vector<uint8_t> ret;
+
+		while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
+			char_array_4[i++] = encoded_string[in_]; in_++;
+			if (i == 4) {
+				for (i = 0; i <4; i++)
+					char_array_4[i] = (uint8_t)base64_chars.find(char_array_4[i]);
+
+				char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+				char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+				char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+				for (i = 0; (i < 3); i++)
+					ret.push_back(char_array_3[i]);
+				i = 0;
+			}
+		}
+
+		if (i) {
+			for (j = i; j <4; j++)
+				char_array_4[j] = 0;
+
+			for (j = 0; j <4; j++)
+				char_array_4[j] = (uint8_t)base64_chars.find(char_array_4[j]);
+
+			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+			char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+			for (j = 0; (j < i - 1); j++) ret.push_back(char_array_3[j]);
+		}
+
+		return ret;
+	}
+}
+
+// CreatureMetaData
 CreatureModule::CreatureMetaData::CreatureMetaData(const std::string& json_str)
 {
     is_valid = false;
@@ -41,6 +97,23 @@ CreatureModule::CreatureMetaData::CreatureMetaData(const std::string& json_str)
         }
 
         return ret_node;
+    };
+
+    auto getJsonNodeFromArray = [this](JsonNode& json_obj, int idx)
+    {
+        int i = 0;
+        for(auto it = JsonBegin(json_obj.value); it != JsonEnd(json_obj.value); ++it)
+        {
+            JsonNode * cur_node = *it;
+            if(i == idx)
+            {
+                return cur_node;
+            }
+
+            i++;
+        }
+
+        return (JsonNode *)nullptr;
     };
 
     auto parseMeshes = [&](JsonNode * node_in)
@@ -122,6 +195,69 @@ CreatureModule::CreatureMetaData::CreatureMetaData(const std::string& json_str)
         }
     };
 
+    auto parseMorphTargets = [this, getJsonNode, getJsonNodeFromArray]
+    (
+        JsonNode * node_morph_targets, 
+        JsonNode * node_morph_res, 
+        JsonNode * node_morph_space)
+    {
+		auto morph_obj = node_morph_targets;
+
+		auto morph_center_array = getJsonNode(*morph_obj, "CenterData");
+		morph_data.center_clip = getJsonNodeFromArray(*morph_center_array, 1)->value.toString();
+
+		auto morph_shapes_array = getJsonNode(*morph_obj, "MorphShape");
+        int shapes_array_size = 0;
+		morph_data.bounds_min = glm::vec2(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+		morph_data.bounds_max = glm::vec2(std::numeric_limits<float>::min(), std::numeric_limits<float>::min());
+
+        for(auto it = JsonBegin(morph_shapes_array->value); it != JsonEnd(morph_shapes_array->value); ++it)
+		{
+			auto cur_array = *it;
+			auto cur_clip = getJsonNodeFromArray(*cur_array, 0)->value.toString();
+			auto pts_array = getJsonNodeFromArray(*cur_array, 1);
+			glm::vec2 cur_pt(
+                (float)getJsonNodeFromArray(*pts_array, 0)->value.toNumber(), 
+                (float)getJsonNodeFromArray(*pts_array, 1)->value.toNumber());
+
+			morph_data.bounds_min.x = std::min(morph_data.bounds_min.x, cur_pt.x);
+			morph_data.bounds_max.x = std::max(morph_data.bounds_max.x, cur_pt.x);
+			morph_data.bounds_min.y = std::min(morph_data.bounds_min.y, cur_pt.y);
+			morph_data.bounds_max.y = std::max(morph_data.bounds_max.y, cur_pt.y);
+
+			morph_data.morph_clips.push_back(std::make_pair(cur_clip, cur_pt));
+            shapes_array_size++;
+		}
+		
+		morph_data.morph_res = static_cast<int>(node_morph_res->value.toNumber());
+		{
+			// Transform to Image space
+			for (auto& cur_clip : morph_data.morph_clips)
+			{
+				auto bounds_size = morph_data.bounds_max - morph_data.bounds_min;
+				auto& cur_pt = cur_clip.second;
+				cur_pt = (cur_pt - morph_data.bounds_min) / bounds_size * (float)(morph_data.morph_res - 1);
+				cur_pt.x = std::min(std::max(0.0f, cur_pt.x), (float)(morph_data.morph_res - 1));
+				cur_pt.y = std::min(std::max(0.0f, cur_pt.y), (float)(morph_data.morph_res - 1));
+			}
+		}
+
+
+		std::string raw_str = node_morph_space->value.toString();
+		auto raw_bytes = Base64Lib::base64_decode(raw_str);
+		for (size_t j = 0; j < shapes_array_size; j++)
+		{
+			int space_size = morph_data.morph_res * morph_data.morph_res;
+			int byte_idx = j * space_size;
+			std::vector<uint8_t> space_data;
+			space_data.resize(space_size);
+			std::memcpy(space_data.data(), raw_bytes.data() + byte_idx, space_size);
+			morph_data.morph_spaces.push_back(space_data);
+		}
+
+		morph_data.weights.resize(shapes_array_size);        
+    };
+
     // Load in MetaData types
     JsonDataHelper json_data;
     auto status = loadJsonFromStr(json_str, json_data);
@@ -133,6 +269,7 @@ CreatureModule::CreatureMetaData::CreatureMetaData(const std::string& json_str)
     } 
 
     JsonNode * json_base = json_data.base_node.toNode();
+    JsonNode * node_morph_targets = nullptr, * node_morph_res = nullptr, * node_morph_space = nullptr;
     while(json_base->next != nullptr)
     {
         auto cur_key = std::string(json_base->key);
@@ -153,8 +290,26 @@ CreatureModule::CreatureMetaData::CreatureMetaData(const std::string& json_str)
         {
             parseSkinSwapList(cur_node);
         }        
+        else if(cur_key == "MorphTargets")
+        {
+            node_morph_targets = cur_node;           
+        }
+        else if(cur_key == "MorphRes")
+        {
+            node_morph_res = cur_node;
+        }
+        else if(cur_key == "MorphSpace")
+        {
+            node_morph_space = cur_node;           
+        }
 
         json_base = json_base->next;
+    }
+
+    // Morph Targets Parsing
+    if(node_morph_targets && node_morph_res && node_morph_space)
+    {
+        parseMorphTargets(node_morph_targets, node_morph_res, node_morph_space);
     }
 
     is_valid = true;
@@ -279,6 +434,84 @@ void CreatureModule::CreatureMetaData::updateIndicesAndPoints(
         // Nothing changded, just copy from source
         copyIndices(src_indices, 0, num_indices);
     }
+}
+
+void CreatureModule::CreatureMetaData::updateMorphStep(
+    CreatureModule::CreatureManager * manager_in, 
+    float delta_step)
+{
+	auto creature_in = manager_in->GetCreature();
+	if (morph_data.play_anims_data.empty())
+	{
+		auto& all_clips = manager_in->GetAllAnimations();
+		morph_data.play_anims_data.resize(morph_data.morph_clips.size());
+		for (size_t i = 0; i < morph_data.play_anims_data.size(); i++)
+		{
+			auto& cur_play_data = morph_data.play_anims_data[i];
+			cur_play_data.first = morph_data.morph_clips[i].first;
+			const auto& cur_clip_name = cur_play_data.first;
+			cur_play_data.second = all_clips[cur_clip_name]->getStartTime();
+		}
+
+		if (morph_data.center_clip.size() > 0)
+		{
+			morph_data.play_center_anim_data.first = morph_data.center_clip;
+			const auto& center_clip_name = morph_data.play_center_anim_data.first;
+			morph_data.play_center_anim_data.second = all_clips[center_clip_name]->getStartTime();
+		}
+
+		morph_data.play_pts.resize(creature_in->GetTotalNumPoints() * 3);
+	}
+
+    std::memset(morph_data.play_pts.data(), 0, sizeof(glm::float32) * morph_data.play_pts.size());
+
+	auto updatePoints = [this, creature_in](float ratio_in)
+	{
+		auto render_pts = morph_data.play_pts.data();
+		for (int j = 0; j < creature_in->GetTotalNumPoints() * 3; j += 3)
+		{
+			render_pts[j] +=
+				(creature_in->GetRenderPts()[j] * ratio_in);
+			render_pts[j + 1] +=
+				(creature_in->GetRenderPts()[j + 1] * ratio_in);
+		}
+	};
+
+	float center_ratio = 0;
+	bool has_center = (morph_data.center_clip.size() > 0);
+	if (has_center)
+	{
+		auto radius = (float)morph_data.morph_res * 0.5f;
+		auto test_pt = morph_data.play_img_pt - glm::vec2(morph_data.morph_res / 2, morph_data.morph_res / 2);
+		center_ratio = glm::length(test_pt / ((float)morph_data.morph_res * 0.5f));
+
+		const auto& clip_name = morph_data.play_center_anim_data.first;
+		manager_in->SetActiveAnimationName(clip_name);
+		manager_in->setRunTime(morph_data.play_center_anim_data.second);
+		manager_in->Update(delta_step);
+
+		float inv_center_ratio = 1.0f - center_ratio;
+		updatePoints(inv_center_ratio);
+		morph_data.play_center_anim_data.second = manager_in->getRunTime();
+	}
+
+	for (size_t i = 0; i < morph_data.play_anims_data.size(); i++)
+	{
+		auto& cur_data = morph_data.play_anims_data[i];
+		const auto& clip_name = cur_data.first;
+		manager_in->SetActiveAnimationName(clip_name);
+		manager_in->setRunTime(cur_data.second);
+		manager_in->Update(delta_step);
+
+		cur_data.second = manager_in->getRunTime();
+		updatePoints((center_ratio > 0) ? (morph_data.weights[i] * center_ratio) : morph_data.weights[i]);
+	}
+
+	// Copy to current render points
+	std::memcpy(
+		creature_in->GetRenderPts(),
+		morph_data.play_pts.data(), 
+		sizeof(glm::float32) * morph_data.play_pts.size());
 }
 
 const std::vector<int> * 
--- creaturegodot3/creaturegodot/CreatureMetaData.h
+++ creaturegodot3/creaturegodot/CreatureMetaData.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <functional>
+#include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
@@ -8,6 +9,10 @@
 #include "glm.hpp"
 
 class meshRenderBoneComposition;
+
+namespace CreatureModule {
+    class CreatureManager;
+}
 
 namespace CreatureModule
 {
@@ -46,6 +51,88 @@ namespace CreatureModule
 
         std::string runEvents(const std::string& anim_name, int time_in);
 
+    	void computeMorphWeights(const glm::vec2& img_pt)
+    	{
+    		morph_data.play_img_pt = img_pt;
+    		auto sampleFilterPt = [](
+    				float q11, // (x1, y1)
+    				float q12, // (x1, y2)
+    				float q21, // (x2, y1)
+    				float q22, // (x2, y2)
+    				float x1,
+    				float y1,
+    				float x2,
+    				float y2,
+    				float x,
+    				float y)
+    		{
+    			float x2x1, y2y1, x2x, y2y, yy1, xx1;
+    			x2x1 = x2 - x1;
+    			y2y1 = y2 - y1;
+    			x2x = x2 - x;
+    			y2y = y2 - y;
+    			yy1 = y - y1;
+    			xx1 = x - x1;
+
+    			float denom = (x2x1 * y2y1);
+    			float numerator = (
+    				q11 * x2x * y2y +
+    				q21 * xx1 * y2y +
+    				q12 * x2x * yy1 +
+    				q22 * xx1 * yy1
+    				);
+
+    			return (denom == 0) ? q11 : (1.0f / denom * numerator);
+    		};
+
+    		auto lookupVal = [this](int x_in, int y_in, int idx)
+    		{
+    			auto& cur_space = morph_data.morph_spaces[idx];
+    			return static_cast<float>(cur_space[y_in *  morph_data.morph_res + x_in]) / 255.0f;
+    		};
+
+    		float x1 = std::floor(img_pt.x);
+    		float y1 = std::floor(img_pt.y);
+    		float x2 = std::ceil(img_pt.x);
+    		float y2 = std::ceil(img_pt.y);
+
+    		for (int i = 0; i < static_cast<int>(morph_data.morph_spaces.size()); i++)
+    		{
+    			float q11 = (float)lookupVal(x1, y1, i); // (x1, y1)
+    			float q12 = (float)lookupVal(x1, y2, i); // (x1, y2)
+    			float q21 = (float)lookupVal(x2, y1, i); // (x2, y1)
+    			float q22 = (float)lookupVal(x2, y2, i); // (x2, y2)
+
+    			float sample_val = sampleFilterPt(
+    				q11, q12, q21, q22, x1, y1, x2, y2, img_pt.x, img_pt.y);
+    			morph_data.weights[i] = sample_val;
+    		}
+    	}
+
+    	void computeMorphWeightsNormalised(const glm::vec2& normal_pt)
+    	{
+    		auto img_pt = normal_pt * glm::vec2(morph_data.morph_res - 1, morph_data.morph_res - 1);
+    		img_pt.x = std::max(std::min((float)morph_data.morph_res - 1.0f, img_pt.x), 0.0f);
+    		img_pt.y = std::max(std::min((float)morph_data.morph_res - 1.0f, img_pt.y), 0.0f);
+    		computeMorphWeights(img_pt);
+    	}
+
+    	void computeMorphWeightsWorld(const glm::vec2& world_pt, const glm::vec2& base_pt, float radius)
+    	{
+    		auto rel_pt = world_pt - base_pt;
+    		auto cur_length = glm::length(rel_pt);
+    		if (cur_length > radius)
+    		{
+    			rel_pt = glm::normalize(rel_pt);
+    			rel_pt *= radius;
+    		}
+
+    		glm::vec2 normal_pt = (rel_pt + glm::vec2(radius, radius)) / (radius * 2.0f);
+    		computeMorphWeightsNormalised(normal_pt);
+    	}
+
+    	void updateMorphStep(CreatureModule::CreatureManager * manager_in, float delta_step);        
+
     protected:
 
         const std::vector<int> * sampleOrder(const std::string& anim_name, int time_in) const;
@@ -55,5 +142,25 @@ namespace CreatureModule
         std::unordered_map<std::string, std::unordered_map<int, std::pair<std::string, bool>>> anim_events_map;
         std::unordered_map<std::string, std::unordered_set<std::string>> skin_swaps;
         bool is_valid;
+
+    	struct MorphData
+    	{
+    		std::vector<std::vector<uint8_t>> morph_spaces;
+    		std::string center_clip;
+    		std::vector<std::pair<std::string, glm::vec2>> morph_clips;
+    		std::vector<float> weights;
+    		glm::vec2 bounds_min, bounds_max;
+    		int morph_res;
+    		std::vector<std::pair<std::string, float>> play_anims_data;
+    		std::pair<std::string, float> play_center_anim_data;
+    		std::vector<glm::float32> play_pts;
+    		glm::vec2 play_img_pt;
+
+    		bool isValid() const {
+    			return (morph_spaces.empty() == false);
+    		}
+    	};
+    	MorphData morph_data;
+
     };
 }
